70fe653b2af9993de32cb56524907fa8e821395d
Ekaterina Tyulkina <8marta_08@mail.ru>
2019-04-14 23:11:47 +0300

    добавила config gunicorn и wsgi-application в файле hello

---
+++ hello.py
@@ -0,0 +1,7 @@
+def application(environ,start_response):
+    data = '\n'.join(environ['QUERY_STRING'].split('&'))
+    data = data.encode()
+    status = '200 OK'
+    headers = [('Content-Type','text/plain')]
+    start_response(status,headers)
+    return list(data)
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo /etc/init.d/nginx restart
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:application

ae742f8ecb43ba02322e89d5e382b34c48a3f3e2
Ekaterina <8marta_08@mail.ru>
2020-11-05 21:37:14 +0300

    2.1 django

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
+sudo ln -sf /home/ekaterina/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:application
+sudo gunicorn -c /home/ekaterina/box/web/etc/gunicorn.conf hello:application
+sudo gunicorn -c /home/ekaterina/box/web/etc/gunicorn_django.conf ask.ask.wsgi:application
+
+

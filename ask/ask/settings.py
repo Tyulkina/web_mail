95a73a0ee6e7269c985abe0a7b481b7ae1b54afa
Ekaterina Tyulkina <8marta_08@mail.ru>
2020-11-26 22:46:49 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ask.qa'
+    'qa'
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ask.ask.urls'
+ROOT_URLCONF = 'ask.urls'
 
 TEMPLATES = [
     {

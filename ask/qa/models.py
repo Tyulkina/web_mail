22dc2a7d746e58e6f1f273691ad3db9673195286
Ekaterina Tyulkina <8marta_08@mail.ru>
2020-11-26 22:55:30 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -11,19 +11,19 @@ class QuestionManager(models.Manager):
  
 
 class Question(models.Model):
-    title = models.CharField(max_length=255) #заголовок вопроса
-    text = models.TextField() #полный текст вопроса
-    added_at = models.DateTimeField() #дата добавления вопроса
-    rating = models.IntegerField() #рейтинг вопроса (число)
-    author = models.CharField(max_length=255) #автор вопроса
-    likes = models.ManyToManyField(User) #список пользователей, поставивших "лайк"
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.CharField(max_length=255)
+    likes = models.ManyToManyField(User)
     objects = QuestionManager()
     
 class Answer(models.Model):    
     text = models.TextField() #текст ответа
-    added_at = models.DateTimeField(auto_now_add = True) #дата добавления ответа
-    question = models.ForeignKey(Question, on_delete = models.PROTECT) #вопрос, к которому относится ответ
-    author = models.ForeignKey(User, on_delete = models.PROTECT) #автор ответа    
+    added_at = models.DateTimeField(auto_now_add = True)
+    question = models.ForeignKey(Question, on_delete = models.PROTECT)
+    author = models.ForeignKey(User, on_delete = models.PROTECT)
  
  
 

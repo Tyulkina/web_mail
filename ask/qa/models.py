4133d744769a923faa0f82815ce75f35526b0fa0
Ekaterina <8marta_08@mail.ru>
2020-11-26 23:20:08 +0300

    fix in models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,29 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class QuestionManager(models.Manager):
-    def new(self): #метод, возвращающий последние добавленные вопросы
+    def new(self):
     	return super(QuestionManager, self).get_query_set().order_by('-added_at')
     
     
-    def popular(): #метод, возвращающий вопросы отсортированные по рейтингу
+    def popular():
     	return super(QuestionManager, self).get_query_set().order_by('-rating')    
  
 
 class Question(models.Model):
-    title = models.CharField(max_length=255) #заголовок вопроса
-    text = models.TextField() #полный текст вопроса
-    added_at = models.DateTimeField() #дата добавления вопроса
-    rating = models.IntegerField() #рейтинг вопроса (число)
-    author = models.CharField(max_length=255) #автор вопроса
-    likes = models.ManyToManyField(User) #список пользователей, поставивших "лайк"
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, on_delete = models.PROTECT),
+    likes = models.ManyToManyField(User,related_name='question_likes_user')
     objects = QuestionManager()
     
 class Answer(models.Model):    
-    text = models.TextField() #текст ответа
-    added_at = models.DateTimeField(auto_now_add = True) #дата добавления ответа
-    question = models.ForeignKey(Question, on_delete = models.PROTECT) #вопрос, к которому относится ответ
-    author = models.ForeignKey(User, on_delete = models.PROTECT) #автор ответа    
- 
- 
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add = True)
+    question = models.ForeignKey(Question, on_delete = models.PROTECT)
+    author = models.ForeignKey(User, on_delete = models.PROTECT)
 
 
